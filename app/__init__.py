86ca951cbfaffea1ea6e2c645cf02483fbb6693f
sunil-sujanix <sunil@sujanix.com>
2025-09-20 20:25:27 +0530

    Adding Migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
@@ -13,6 +13,18 @@ db=SQLAlchemy(app)
 
 migrate=Migrate(app,db)
 
+
+
+
 from app import routes,models
+from app.models import Subscription
+
+
+
+@app.route('/getemails')
+def emails():
+    res=Subscription.query.all()
+    return jsonify({'data':[{"id":j.id,"email":j.email }for j in res]})
+
 
 
