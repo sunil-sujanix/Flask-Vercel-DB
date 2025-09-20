3daad5ac8fb28260da3f2506aaf5324eb10bde09
sunil-sujanix <sunil@sujanix.com>
2025-09-20 20:21:37 +0530

    Adding Migrations

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,url_for,flash,redirect
+from flask import render_template,request,url_for,flash,redirect,jsonify
 from app import app,db
 from app.models import Subscription
 
@@ -7,6 +7,12 @@ from app.models import Subscription
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/getemails')
+def emails():
+    res=Subscription.query.all()
+    return jsonify({'data':[{"id":j.id,"email":j.email }for j in res]})
 
 
 
