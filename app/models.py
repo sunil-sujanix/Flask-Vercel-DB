be8aec2f967582989c043557b4e3003b4d6405ec
sunil-sujanix <sunil@sujanix.com>
2025-09-20 19:44:04 +0530

    Adding Migrations

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 from datetime import datetime
 
 class Subscription(db.Model):
-    id=db.Column(db.integer,primary_key=True)
+    id=db.Column(db.Integer,primary_key=True)
     email=db.Column(db.String(255),unique=True,nullable=False)
     subscribed_at=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
     
--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     SECRET_KEY=os.getenv('SECRET_KEY',os.urandom(24))
-    SQLALCHEMY_DATABASE_URI=os.getenv('Database_URL','post')
+    SQLALCHEMY_DATABASE_URI=os.getenv('Database_URL',"postgresql://neondb_owner:npg_F5GWma9cEBIK@ep-small-forest-ad1245c3-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require")
     SQLALCHEMY_TRACK_MODIFICATIONS=False
