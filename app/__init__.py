ac3dbf72599c937f17eefc768a06df9b672914b4
sunil-sujanix <sunil@sujanix.com>
2025-09-22 10:01:12 +0530

    Adding Migrations

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
 
     db.init_app(app)
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 from app import create_app
 
+
 app = create_app()  # ✅ this is what Vercel will serve
